b8b459df6a45ee895e82b7185a5751d4d7584382
Thales Henrique <thalespilao@hotmail.com>
2026-09-14 01:16:02 +0100

    added some fonts and loading it into the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,35 @@
+import { Baloo2_700Bold, useFonts } from '@expo-google-fonts/baloo-2';
+import { Nunito_400Regular } from '@expo-google-fonts/nunito';
+import { } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
 import '../global.css';
 
 export { ErrorBoundary } from 'expo-router';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Baloo2: Baloo2_700Bold,
+    Nunito: Nunito_400Regular,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [loaded]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,46 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#58CC02',
+          shade: '#48A800',
+        },
+        secondary: {
+          DEFAULT: '#FF6B6B',
+          shade: '#E63946',
+        },
+        success: {
+          DEFAULT: '#06D6A0',
+          shade: '#048A81',
+        },
+        warning: {
+          DEFAULT: '#FFD166',
+          shade: '#F0A500',
+        },
+        destructive: {
+          DEFAULT: '#EF476F',
+          shade: '#C1121F',
+        },
+        neutral: {
+          50: '#F9FAFB',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+        }
+      },
+      fontFamily: {
+        display: ['Baloo2'],
+        body: ['Nunito'],
+      }
+    },
   },
   plugins: [],
 }
